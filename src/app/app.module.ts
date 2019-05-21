0b9f017a51ee49902ef2797cc5d65d8a08ab4178
skusVV <skus1992@gmail.com>
2019-05-21 09:40:39 +0300

    created form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule, MatNativeDateModule, MatInputModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from '../components/form/form.component';
@@ -10,7 +16,15 @@ import { FormComponent } from '../components/form/form.component';
     FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/form/form.component.ts
+++ src/components/form/form.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+interface ISelect {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-form',
@@ -7,4 +13,38 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
   title = 'New BT request';
+
+  startDate: FormControl = new FormControl('');
+  endDate: FormControl = new FormControl('');
+  // location: FormControl = new FormControl('');
+  // office: FormControl = new FormControl('');
+  trainTickets: ISelect[] = [
+    {value: '1', viewValue: 'train 1'},
+    {value: '2', viewValue: 'train 2'},
+    {value: '3', viewValue: 'train 3'},
+  ];
+
+  plainTickets: ISelect[] = [
+    {value: '1', viewValue: 'plain 1'},
+    {value: '2', viewValue: 'plain 2'},
+    {value: '3', viewValue: 'plain 3'},
+  ];
+
+  locations: ISelect[] = [
+    {value: '1', viewValue: 'location 1'},
+    {value: '2', viewValue: 'location 2'},
+    {value: '3', viewValue: 'location 3'}
+  ];
+
+  offices: ISelect[] = [
+    {value: '1', viewValue: 'office 1'},
+    {value: '2', viewValue: 'office 2'},
+    {value: '3', viewValue: 'office 3'}
+  ];
+
+  hotels: ISelect[] = [
+    {value: '1', viewValue: 'hotel 1'},
+    {value: '2', viewValue: 'hotel 2'},
+    {value: '3', viewValue: 'hotel 3'}
+  ];
 }
